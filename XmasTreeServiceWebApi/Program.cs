f754b91bd9a5f368c3f410fd95fbf6aa8985c9d2
rafalswirk <rafalswirk@outlook.com>
2022-12-09 15:38:14 +0100

    - Setting up project startup

--- XmasTreeServiceWebApi/Program.cs
+++ XmasTreeServiceWebApi/Program.cs
@@ -1,25 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace XmasTreeServiceWebApi;
 
-// Add services to the container.
+ public class Program
+    {
+        public static Task Main(string[] args) 
+            => CreateHostBuilder(args).Build().RunAsync();
+        
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
---
+++ XmasTreeServiceWebApi/Startup.cs
@@ -0,0 +1,38 @@
+namespace XmasTreeServiceWebApi;
+
+public class Startup
+{   
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Confab.Bootstrapper v1"));
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapGet("/", context => context.Response.WriteAsync("XmasTree API"));
+        });
+    }
+
+}

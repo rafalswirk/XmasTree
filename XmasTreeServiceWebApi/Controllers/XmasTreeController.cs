871de0d7b357c032a785846ceea2d3389ff136d6
rafalswirk <rafalswirk@outlook.com>
2022-12-14 18:20:11 +0100

    - Added empty controller for XmasTree

---
+++ XmasTreeServiceWebApi/Controllers/XmasTreeController.cs
@@ -0,0 +1,11 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XmasTreeServiceWebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class XmasTreeController : ControllerBase
+{
+    
+}

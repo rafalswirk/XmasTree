eee7b40b11b39a8da0cec350f26ca568b2ce9d08
rafalswirk <rafalswirk@outlook.com>
2022-12-16 06:16:16 +0100

    - Preparing client app using Maui framework

---
+++ XmasTreeApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace XmasTreeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

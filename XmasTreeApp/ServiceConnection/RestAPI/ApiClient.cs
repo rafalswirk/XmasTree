38d4c064a82ddc75a7aeeb7889d989d9e5d9ab9a
rafalswirk <rafalswirk@outlook.com>
2022-12-16 10:53:50 +0100

    - Preparing layout for the main page

---
+++ XmasTreeApp/ServiceConnection/RestAPI/ApiClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using XmasTreeApp.ServiceConnection.Dto;
+
+namespace XmasTreeApp.ServiceConnection.RestAPI
+{
+    class ApiClient
+    {
+        private readonly HttpClient _client;
+        private readonly string _baseUrl;
+
+        public ApiClient(string url)
+        {
+            _client = new HttpClient();
+            _baseUrl = url;
+        }
+
+        public async Task<IEnumerable<LightModeDto>> GetData()
+        {
+            //Uri uri = new Uri("http://192.168.0.161:5000/xmastree");
+            Uri uri = new Uri($"{_baseUrl}/xmastree");
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    var items = JsonSerializer.Deserialize<List<string>>(content);
+                    return items.Select(i => new LightModeDto { Id = i}).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+                return null;
+            }
+            return null;
+        }
+    }
+}
